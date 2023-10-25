e58b4500e2336101f9a9c867383afbf276dadf10
IrinaEnotova <irinkayashina@gmail.com>
2023-10-25 08:28:53 +0400

    feat: add styles for items wrapper

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,21 @@
 import { Component, ReactNode } from 'react';
+import styles from './Content.module.css';
+import Item from '../Item/Item';
 
 class Content extends Component {
   render(): ReactNode {
-    return <div>Content</div>;
+    return (
+      <div className={styles['wrapper']}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+    );
   }
 }
 
