3060cc43b7c8a08595c72903530edcb493a88930
IrinaEnotova <irinkayashina@gmail.com>
2023-10-28 16:20:43 +0400

    refactor: wrap component App to an error boundary to catch errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Component, ReactNode } from 'react';
 import CharactersPage from './pages/CharactersPage/CharactersPage';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
   render(): ReactNode {
-    return (
-      <ErrorBoundary>
-        <CharactersPage />;
-      </ErrorBoundary>
-    );
+    return <CharactersPage />;
   }
 }
 
