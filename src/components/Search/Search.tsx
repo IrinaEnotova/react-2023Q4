a6696d96cb72498f68ab825c0359335d06cac6ea
IrinaEnotova <irinkayashina@gmail.com>
2023-11-01 19:07:09 +0400

    refactor: move throwError btn to Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,41 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import SearchProps from './Search.props';
 import Button from '../Button/Button';
 import styles from './Search.module.css';
 
 const Search = ({ searchQuery, handleChange, getSearch }: SearchProps) => {
+  const [isErrorBoundary, setIsErrorBoundary] = useState(false);
   const submit = (event: FormEvent) => {
     event.preventDefault();
     getSearch();
   };
 
+  const throwErrorBoundary = () => {
+    setIsErrorBoundary(true);
+  };
+
+  if (isErrorBoundary) {
+    throw new Error('ErrorBoundary worked!');
+  }
+
   return (
-    <form className={styles['wrapper']} onSubmit={submit}>
-      <input
-        className={styles['input']}
-        type="text"
-        value={searchQuery}
-        placeholder="Search by name"
-        onChange={(event) => {
-          handleChange(event.target.value);
-        }}
-      />
-      <Button>Search</Button>
-    </form>
+    <div className={styles['wrapper']}>
+      <form className={styles['form']} onSubmit={submit}>
+        <input
+          className={styles['input']}
+          type="text"
+          value={searchQuery}
+          placeholder="Search by name"
+          onChange={(event) => {
+            handleChange(event.target.value);
+          }}
+        />
+        <Button className={styles['search-btn']}>Search</Button>
+      </form>
+      <Button className={styles['error-btn']} onClick={throwErrorBoundary}>
+        Throw error
+      </Button>
+    </div>
   );
 };
 
