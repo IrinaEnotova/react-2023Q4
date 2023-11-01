b65b6fcd90544dc94a6069ec54aabe7fc787ed17
IrinaEnotova <irinkayashina@gmail.com>
2023-11-01 22:10:02 +0400

    refactor: add btn for errorBlock to navigate to first page

--- src/components/ErrorBlock/ErrorBlock.tsx
+++ src/components/ErrorBlock/ErrorBlock.tsx
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../Button/Button';
 import styles from './ErrorBlock.module.css';
 
 const ErrorBlock = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles['error-block']}>
-      <img className={styles['img']} src="item-icon.svg" alt="Error" />
+      <img className={styles['img']} src="/item-icon.svg" alt="Error" />
       <h1>
         Something went wrong <br />
         ErrorBoundary worked!
       </h1>
+      <Button
+        onClick={() => {
+          navigate('/page/1');
+        }}
+      >
+        To first page
+      </Button>
     </div>
   );
 };
