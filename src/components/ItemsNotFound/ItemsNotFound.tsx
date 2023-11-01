0f31135e41295799f75a186e1ded6f2981e383c4
IrinaEnotova <irinkayashina@gmail.com>
2023-11-01 22:12:37 +0400

    fix: update styles for ItemsNotFound

--- src/components/ItemsNotFound/ItemsNotFound.tsx
+++ src/components/ItemsNotFound/ItemsNotFound.tsx
@@ -3,7 +3,7 @@ import styles from './ItemsNotFound.module.css';
 const ItemsNotFound = () => {
   return (
     <div className={styles['wrapper']}>
-      <img src="./r2-d2.svg" alt="Items not found" />
+      <img src="/item-icon.svg" alt="Items not found" />
       <h2 className={styles['not-found']}>
         Characters were not found! <br /> Enter another name
       </h2>
