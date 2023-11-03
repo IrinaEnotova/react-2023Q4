95240313ef84cdcdb997ac66d3069252a6711757
IrinaEnotova <irinkayashina@gmail.com>
2023-11-03 13:07:02 +0400

    refactor: add function for update searchParams

--- src/components/ItemList/ItemList.props.ts
+++ src/components/ItemList/ItemList.props.ts
@@ -2,6 +2,7 @@ import ApiItem from '../../interfaces/interfaces';
 
 interface ItemListProps {
   items: ApiItem[];
+  changeSearchParams: (characterId: string) => void;
 }
 
 export default ItemListProps;
--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -2,11 +2,11 @@ import Item from '../Item/Item';
 import ItemListProps from './ItemList.props';
 import styles from './ItemList.module.css';
 
-const ItemList = ({ items }: ItemListProps) => {
+const ItemList = ({ items, changeSearchParams }: ItemListProps) => {
   return (
     <div className={styles['wrapper']}>
       {items.map((item) => (
-        <Item key={item._id} item={item} />
+        <Item key={item._id} item={item} changeSearchParams={changeSearchParams} />
       ))}
     </div>
   );
