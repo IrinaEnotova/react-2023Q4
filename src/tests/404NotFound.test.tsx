a0baef28f399b108b26c627b2c26c2208a8f825e
IrinaEnotova <irinkayashina@gmail.com>
2023-11-17 15:21:36 +0400

    refactor: update tests for 404 and utils

--- src/tests/404NotFound.test.tsx
+++ src/tests/404NotFound.test.tsx
@@ -1,21 +1,17 @@
 import { screen } from '@testing-library/react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { AppContextProvider } from '../context/AppContext';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import CharactersPage from '../pages/CharactersPage/CharactersPage';
 import NotFound from '../components/NotFound/NotFound';
+import { Provider } from 'react-redux';
+import { setupStore } from '../store/store';
 
 describe('404 Not Found component', () => {
   test('is displayed when navigating to an invalid route', async () => {
+    const store = setupStore();
     render(
-      <AppContextProvider
-        value={{
-          items: [],
-          searchQuery: '',
-          selectedItemId: '',
-        }}
-      >
+      <Provider store={store}>
         <MemoryRouter initialEntries={['/someinvalidroute']}>
           <Routes>
             <Route path="/" element={<CharactersPage />}>
@@ -24,7 +20,7 @@ describe('404 Not Found component', () => {
             <Route path="*" element={<NotFound>Page was not found</NotFound>}></Route>
           </Routes>
         </MemoryRouter>
-      </AppContextProvider>
+      </Provider>
     );
 
     expect(screen.getByText('Page was not found')).toBeInTheDocument();
